eeffb54180241a3fb439658f06fdfe142130cdc9
Anton Lysogor <itonyjah@gmail.com>
2024-12-07 08:26:55 +0300

    added list throug cycle

--- hello.py
+++ hello.py
@@ -4,3 +4,14 @@ bar += ' and from VSC as well'
 bar += ' and from VSC again. Try push via ssh.'
 bar += ' and from new PyCharm'
 print(foo, bar)
+
+set = []
+i = 0
+while i < 10:
+    set.append(i)
+    i += 1
+print(set)
+
+for i in set:
+    print(i, type(i), set[i])
+print(set)
