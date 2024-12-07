4f72d011b5dc67f7a1f232d31ecb8d89dcd59208
Anton Lysogor <anton.lysogor@gmail.com>
2024-12-07 11:53:35 +0300

    added sting list to initial list

--- hello.py
+++ hello.py
@@ -5,13 +5,19 @@ bar += ' and from VSC again. Try push via ssh.'
 bar += ' and from new PyCharm'
 print(foo, bar)
 
-set = []
+set_list = []
 i = 0
-while i < 10:
-    set.append(i)
+while i < 3:
+    set_list.append(i)
     i += 1
-print(set)
+print(set_list)
 
-for i in set:
-    print(i, type(i), set[i])
-print(set)
+for i in set_list:
+    print(i, type(i), set_list[i])
+print(set_list)
+
+Anton = ['A', 'n', 't', 'o', 'n']
+print(Anton)
+set_list.append(Anton)
+set_list.extend(Anton)
+print(set_list)
