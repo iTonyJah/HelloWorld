9370f5059978a50590352ca6c1762a9910ac85d7
Anton Lysogor <itonyjah@gmail.com>
2024-12-16 00:27:08 +0300

    added two funtions. one for greeting, one for square calculating

--- hello.py
+++ hello.py
@@ -10,14 +10,12 @@ i = 0
 while i < 3:
     set_list.append(i)
     i += 1
-print(set_list)
 
 for i in set_list:
     print(i, type(i), set_list[i])
-print(set_list)
 
 Anton = ['A', 'n', 't', 'o', 'n']
-print(Anton)
+
 set_list.append(Anton)
 set_list.extend(Anton)
 print(set_list)
@@ -47,32 +45,32 @@ a = type(int(float('123.5')))
 b = type(int(524 ** 12)) #441345311145345))
 c = type(int('342'))
 d = type(int(float('-17.132')))
-print(a, b, c, d)
 
 name = 'Anton Lysogor'
 cell = +79261086838
 address = 'Oleko Dundicha, 34, 55'
-print(name, cell, address)
 
 e = 11 * 2 ** 2 - 13 / 4 + 7
 p = 2 ** 2
 aa = 11 * p
 bb = 13 / 4
 cc = 7
-print(aa, bb, cc, aa - bb + cc)
-print(e)
 
 n = None
-str = '123.457'
+string = '123.457'
 boo = True
 foo = False
 fract = 1/3
-print(n , type(n), type(int(float(str))), type(int(boo)), type(int(foo)), type(int(fract)))
+print(n , type(n), type(int(float(string))), type(int(boo)), type(int(foo)), type(int(fract)))
 
 def hello():
     name = input("Введите ваше имя: ")
     hello = 'Приветствую вас, ' + name + '!'
     return(hello)
 
-print(hello())
+def sq_hund_calc():
+    a = int(input('Введите длину участка в метрах: '))
+    b = int(input('Введите ширину участка в метрах: '))
+    sq = a * b // 100
+    return("Площадь участка: " + str(sq) + " соток.")
 
