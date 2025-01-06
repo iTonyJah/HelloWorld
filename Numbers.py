24841a6d980d26ae67e7b7642fbcb4769304200e
Anton Lysogor <anton.lysogor@gmail.com>
2025-01-06 10:20:44 +0300

    changedto more fast

--- Numbers.py
+++ Numbers.py
@@ -1,20 +1,29 @@
-numbers = [1]
+numbers = {1}
+multipliers = [1]
 lines = [[1]]
-max_number = 1000000
-print(1, '...', '...', len(str(max_number)), max_number)
+max_number = 10000000000000
+print(1, 1, '...', '...', len(str(max_number)), max_number)
 
 def add_new_line(prime):
     new_line = []
     k = 0
-    while k > -1:
-        product = prime * numbers[k]
+    skip = False
+    while True:
+        product = prime * multipliers[k]
         if product > max_number : break
-        new_line.append(product)
-        numbers.append(product)
-        numbers.sort()
         k += 1
+        new_line.append(product)
+        numbers.add(product)
+        if product > max_number / prime:
+            if skip:
+                continue
+            else:
+                skip = True
+        multipliers.append(product)
+        multipliers.sort()
+
     lines.append(new_line)
-    print(new_line[0], len(new_line), len(numbers), len(str(new_line[-1])), new_line[-1])
+    print(len(lines), new_line[0], len(new_line), len(numbers), len(multipliers), len(str(new_line[-1])), new_line[-1])
     return
 
 for i in range(1, max_number + 1):
