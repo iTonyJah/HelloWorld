78375c2a9143342e84f86fa06353cb6585cc9ca4
Anton Lysogor <itonyjah@gmail.com>
2025-01-08 08:46:33 +0300

    minor changes

--- Numbers.py
+++ Numbers.py
@@ -2,7 +2,8 @@ import bisect
 numbers = {1}
 multipliers = [1]
 lines = [[1]]
-max_number = 10 ** 100
+max_number = int(1.6 * 10 ** 7)
+
 print(1, 1, '...', '...', '...', len(str(max_number)), max_number)
 
 def add_new_line(prime):
@@ -14,9 +15,9 @@ def add_new_line(prime):
         product = prime * multipliers[k]
         if product > max_number : break
         k += 1
-        if k % 100000 == 0:
-            print_empty_line = True
-            print(k, end = ' ')
+        #if k % 10**5 == 0:
+            #print_empty_line = True
+            #print(k, end = ' ')
         new_line.append(product)
         numbers.add(product)
         if product > max_number / prime:
@@ -27,7 +28,7 @@ def add_new_line(prime):
     lines.append(new_line)
     if print_empty_line: print('')
     print(len(lines), new_line[0], len(new_line), len(numbers),
-          str(int(len(numbers) / max_number)) + '%',
+          str(int(len(numbers) / max_number * 100)) + '%',
           len(multipliers), len(str(new_line[-1])), new_line[-1])
 
 
@@ -35,5 +36,5 @@ for i in range(1, max_number + 1):
     if i in numbers: continue
     add_new_line(i)
 
-print(len(numbers), len(lines))
 for line in lines: print(line)
+print('n', len(numbers), 'p', len(lines), 'm', len(multipliers))
