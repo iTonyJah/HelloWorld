30a24747e7562d4345d85b0fa5a9a55472c770e3
Anton Lysogor <anton.lysogor@gmail.com>
2025-01-08 21:13:29 +0300

    new python logic -not very good

--- Numbers.py
+++ Numbers.py
@@ -1,40 +1,29 @@
-import bisect
 numbers = {1}
-multipliers = [1]
 lines = [[1]]
-max_number = int(1 * 10 ** 7)
-
+max_number = int(1 * 10 ** 2)
 print(1, 1, '...', '...', '...', len(str(max_number)), max_number)
 
 def add_new_line(prime):
-    new_line = []
-    k = 0
-    skip = False
-    print_empty_line = False
-    while True:
-        product = prime * multipliers[k]
-        if product > max_number : break
-        k += 1
-        #if k % 10**5 == 0:
-            #print_empty_line = True
-            #print(k, end = ' ')
-        new_line.append(product)
-        numbers.add(product)
-        if product > max_number / prime:
-            if skip: continue
-            else: skip = True
-        multipliers.insert(bisect.bisect_left(multipliers, product), product)
-
+    numbers.add(prime)
+    new_line = [prime]
     lines.append(new_line)
-    if print_empty_line: print('')
+    for line in lines:
+        for number in line:
+            product = prime * number
+            if product > max_number or product in numbers : break
+            new_line.append(product)
+            numbers.add(product)
+    print(new_line)
+    print(lines[-1])
+    new_line.sort()
+    print(new_line)
+    print(lines[-1])
     print(len(lines), new_line[0], len(new_line), len(numbers),
-          str(int(len(numbers) / max_number * 100)) + '%',
-          len(multipliers), len(str(new_line[-1])), new_line[-1])
-
+          str(int(len(numbers) / max_number * 100)) + '%', len(str(new_line[-1])), new_line[-1])
 
 for i in range(1, max_number + 1):
     if i in numbers: continue
     add_new_line(i)
 
-for line in lines: print(line)
-print('n', len(numbers), 'p', len(lines), 'm', len(multipliers))
+for l in lines: print(l)
+print('n', len(numbers), 'p', len(lines))
