ef8cb91c60ee91b740ba7115c8c53b9bc416c6ae
Anton Lysogor <itonyjah@gmail.com>
2025-01-09 08:59:56 +0300

    new logic stub unfinished

--- Numbers.py
+++ Numbers.py
@@ -7,17 +7,31 @@ def add_new_line(prime):
     numbers.add(prime)
     new_line = [prime]
     lines.append(new_line)
+    skip = False
+    self = False
     for line in lines:
+        if skip and line[0] != prime: break
         for number in line:
             product = prime * number
-            if product > max_number or product in numbers : break
-            new_line.append(product)
+            if number == prime :
+                self = True
+                print(self)
+            if product in numbers:
+                print('is', product, prime, numbers, lines, line[0])
+                break
+            if product > max_number:
+                print('max', product, prime, numbers, lines, line[0])
+                skip = True
+                break
+
             numbers.add(product)
-    print(new_line)
-    print(lines[-1])
-    new_line.sort()
-    print(new_line)
-    print(lines[-1])
+            new_line.append(product)
+            new_line.sort()
+
+
+        new_line.sort()
+
+
     print(len(lines), new_line[0], len(new_line), len(numbers),
           str(int(len(numbers) / max_number * 100)) + '%', len(str(new_line[-1])), new_line[-1])
 
