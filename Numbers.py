f971b33016b53e8d84f28a570a34c4e6c07c0527
iTonyJah <wormantson@gmail.com>
2025-01-09 10:38:08 +0300

    new logic without multipliers[], still too slow

--- Numbers.py
+++ Numbers.py
@@ -1,36 +1,29 @@
 numbers = {1}
 lines = [[1]]
-max_number = int(1 * 10 ** 2)
+max_number = int(1 * 10 ** 6)
 print(1, 1, '...', '...', '...', len(str(max_number)), max_number)
 
 def add_new_line(prime):
-    numbers.add(prime)
-    new_line = [prime]
-    lines.append(new_line)
-    skip = False
-    self = False
+    new_line = []
     for line in lines:
-        if skip and line[0] != prime: break
         for number in line:
             product = prime * number
-            if number == prime :
-                self = True
-                print(self)
-            if product in numbers:
-                print('is', product, prime, numbers, lines, line[0])
-                break
             if product > max_number:
-                print('max', product, prime, numbers, lines, line[0])
-                skip = True
                 break
 
             numbers.add(product)
             new_line.append(product)
-            new_line.sort()
+    new_line.sort()
 
+    for number2 in new_line:
+        product = prime * number2
+        if product > max_number:
+            break
+        numbers.add(product)
+        new_line.append(product)
 
-        new_line.sort()
-
+    new_line.sort()
+    lines.append(new_line)
 
     print(len(lines), new_line[0], len(new_line), len(numbers),
           str(int(len(numbers) / max_number * 100)) + '%', len(str(new_line[-1])), new_line[-1])
