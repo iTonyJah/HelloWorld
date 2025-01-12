e241870f82d9ab87a7e26d462094f6d56b47354c
Anton Lysogor <itonyjah@gmail.com>
2025-01-12 23:59:52 +0300

    Numbers added several loops with append() and sort() before insert()

--- Numbers.py
+++ Numbers.py
@@ -1,13 +1,14 @@
 import bisect
 numbers = {1}
 lines = [(1,)]
-max_number = int(2.3 * 10 ** 1)
+max_number = int(161.013 * 10 ** 6)
 print(1, 1, '...', '...', '...', len(str(max_number)), max_number)
 
 def fill_line(prime, line, new_line, sort):
     for number in line:
         product = prime * number
         if product > max_number: break
+        if product in numbers: break
         numbers.add(product)
         if sort:
             bisect.insort_left(new_line, product)
@@ -21,6 +22,38 @@ def add_new_line(prime):
         if line[0] * prime > max_number : break
         new_line = fill_line(prime, line, new_line, sort=False)
     new_line.sort()
+
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+    new_line = fill_line(prime, new_line, new_line, sort=False)
+    new_line.sort()
+
     new_line = fill_line(prime, new_line, new_line, sort=True)
     lines.append(tuple(new_line))
     print(len(lines), new_line[0], len(new_line), len(numbers),
@@ -32,7 +65,5 @@ for i in range(1, max_number + 1):
         add_new_line(i)
 
 lines = tuple(lines)
-
-
 for l in lines: print(l)
 print('n', len(numbers), 'p', len(lines) - 1)
