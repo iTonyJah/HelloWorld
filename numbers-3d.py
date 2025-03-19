ee317d41c2037e7f11d3b9e0cb6d6cf1ddd1b846
Anton Lysogor <itonyjah@gmail.com>
2025-03-19 23:45:05 +0300

    try to build logic still fail

--- numbers-3d.py
+++ numbers-3d.py
@@ -9,8 +9,26 @@ def add_new_prime(new_prime):
     new_prime_line_dimension = [new_prime]
     new_prime_line = [new_prime_line_dimension]
     numbers_list.append(new_prime_line)
-    dim = 1
-    
+    print(numbers_list)
+
+    for prime_line in numbers_list:
+        print(prime_line)
+        for pl_dim in prime_line:
+            print('pl_dim', pl_dim)
+            npl = []
+            for i in pl_dim:
+                print(pl_dim, i)
+                product = i * new_prime
+                if product > max_number: break
+                print(product)
+                numbers_set.add(product)
+                npl.append(product)
+                print('npl', npl)
+            if npl: new_prime_line.append(npl)
+
+    numbers_list.append(new_prime_line)
+    print(numbers_set)
+
 
 for n in range(1, max_number + 1):
     if n not in numbers_set:
