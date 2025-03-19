7624b7345fafbe324c4c07bd15744e745d535816
Anton Lysogor <itonyjah@gmail.com>
2025-03-19 09:54:10 +0300

    small changes in numbers-3d, still not working

--- numbers-3d.py
+++ numbers-3d.py
@@ -4,19 +4,12 @@ numbers_list = []
 max_number = int(1 * 10 ** 2)
 
 
-def fill_the_line(line):
-    ...
-
-
 def add_new_prime(new_prime):
     numbers_set.add(new_prime)
     new_prime_line_dimension = [new_prime]
     new_prime_line = [new_prime_line_dimension]
-
     numbers_list.append(new_prime_line)
 
-
-    fill_the_line(new_prime_line)
 
 
 for i in range(1, max_number + 1):
@@ -25,4 +18,4 @@ for i in range(1, max_number + 1):
 
 numbers_list = tuple(numbers_list)
 print(numbers_list)
-print('n', len(numbers_set), 'p', len(numbers_list) - 1)
+print('n', len(numbers_set), 'p', len(numbers_list))
