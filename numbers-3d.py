6b1c7ff6aa7d4395cf74fcc7b8beaf823cd39b08
Anton Lysogor <itonyjah@gmail.com>
2025-03-21 11:53:35 +0300

    changed logic not work

--- numbers-3d.py
+++ numbers-3d.py
@@ -4,30 +4,31 @@ numbers_list = []
 max_number = int(1 * 10 ** 2)
 
 
+def fill_prime_line(prime, prime_line):
+    for p_line in numbers_list:
+        for pl_dim in p_line:
+            npl = []
+            for i in pl_dim:
+                product = i * prime
+                if product > max_number: break
+                numbers_set.add(product)
+                npl.append(product)
+            if npl: prime_line.append(npl)
+            print(prime_line)
+
+    return prime_line
+
+
 def add_new_prime(new_prime):
     numbers_set.add(new_prime)
     new_prime_line_dimension = [new_prime]
     new_prime_line = [new_prime_line_dimension]
     numbers_list.append(new_prime_line)
+    npl = fill_prime_line(new_prime, new_prime_line)
+    numbers_list.pop(-1)
+    numbers_list.append(npl)
+
     print(numbers_list)
-
-    for prime_line in numbers_list:
-        print(prime_line)
-        for pl_dim in prime_line:
-            print('pl_dim', pl_dim)
-            npl = []
-            for i in pl_dim:
-                print(pl_dim, i)
-                product = i * new_prime
-                if product > max_number: break
-                print(product)
-                numbers_set.add(product)
-                npl.append(product)
-                print('npl', npl)
-            if npl: new_prime_line.append(npl)
-
-    numbers_list.append(new_prime_line)
-    print(numbers_set)
 
 
 for n in range(1, max_number + 1):
